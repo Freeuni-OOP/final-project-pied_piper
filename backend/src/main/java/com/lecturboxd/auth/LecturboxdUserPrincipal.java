69d6968c827e6d3ff565e3d9a51ae31a5704d997
lizzmazzo <lizura23@freeuni.edu.ge>
2026-06-24 22:24:57 +0400

    komentari

--- backend/src/main/java/com/lecturboxd/auth/LecturboxdUserPrincipal.java
+++ backend/src/main/java/com/lecturboxd/auth/LecturboxdUserPrincipal.java
@@ -1,1 +1,3 @@
 // Custom UserDetails wrapper attaching LecturBoxd-specific user ID and verification status to the principal.
+// When I had you to myself i didnt want u arouuuuuuuund
+// those pretty faces always made you stand out in the crowd
